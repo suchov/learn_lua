af56baa2676c6473d6079431378bd71c9034b51b
Artem Sychov <[artem.suchov@gmail.com]>
2023-03-12 21:06:59 +0100

    finish simple exercise

---
+++ myscript.lua
@@ -0,0 +1,34 @@
+print("hello world!")
+
+print(12 * 45)
+
+print("Thank you, goodbye!")
+
+print("Examples of variables")
+
+score = 0
+
+lives = 3
+
+player1_name = "Leeroy Jenkins"
+player2_name = "Gustavo Pezzi"
+
+print(type(score))
+print(type(player1_name))
+
+-- Lua has 7 basic types
+-- nil
+-- number
+-- string
+-- function
+-- CFunction
+-- userdata
+-- table
+
+print("I want to join string with number "..score)
+
+print("The value of name is = "..player1_name)
+
+some_number = math.floor(25/2)
+
+print(some_number)
---
+++ simple_exer.lua
@@ -0,0 +1,19 @@
+-----------------
+-- Exercise:
+-- --------
+-- Write a Lua program that receives 
+-- the values of the name and salary
+-- of the user.
+-- The program should calculate and 
+-- display the new salary increased
+-- by 15%
+--
+--
+name = "miles Davis" 
+salary = 500000.00
+
+-- TODO: calculate new_salary
+
+new_salary = salary * 1.15
+
+print("New salary is: ".. new_salary)
