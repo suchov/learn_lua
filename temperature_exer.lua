9762496d61bc15d772e6e03c18183694b92e01c6
Artem Sychov <[artem.suchov@gmail.com]>
2023-03-13 13:43:45 +0100

    home work celsius to fharenheit

---
+++ temperature_exer.lua
@@ -0,0 +1,10 @@
+-- Initialize a variable with any temperature value in Celsius. Your script should
+-- calculate and display the value in Celsius converted to Farhenheit. Assuming TC is the 
+-- temperature in Celsius and TF is the temperature in Fahrenheit, the conversion formula is:
+-- T(Fahrenheit) = 9/5T(Celsius) + 32
+
+Celsius = 15
+
+Temperature = 9/5*Celsius + 32
+
+print("Temperature in Fahrenheit = ".. Temperature)
